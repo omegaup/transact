d6f8632d8edf4aa828aab53f32855e60d3303899
lhchavez <lhchavez@lhchavez.com>
2014-12-28 23:19:13 +0000

    Importing existing code
    This version works and has been tested only on single-processor
    machines. Testing it on SMP would probably be nice, but all omegaUp
    runners are single-processor. Plus, adding memory barriers decreases
    performance significantly, so you still want to set the affinity of all
    processes that use a transactfd to run on a single processor to avoid
    that overhead.

---
+++ README.md
@@ -0,0 +1,5 @@
+# transact
+
+I was bored on Christmas break, so I made a Linux kernel module. This is
+basically a stripped down, named version of an eventfd, with less bells and
+whistles. This should be useful to make libinteractive faster.
---
+++ transact.c
@@ -0,0 +1,276 @@
+/* transact.c 
+ * 
+ * A named semaphore with death notification. Has similar semantics to eventfd,
+ * but only requires a characted device file.
+ *
+ */
+
+/* Standard headers for LKMs */
+#include <linux/module.h>  
+#include <linux/poll.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/cdev.h>
+#include <linux/kref.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/wait.h>
+#include <asm/current.h>
+
+struct transact_ctx;
+struct transact_cdev;
+
+struct transact_proc {
+	struct transact_ctx *ctx;
+	int index;
+};
+
+struct transact_ctx {
+	struct transact_proc child[2];
+	struct transact_cdev *dev;
+	wait_queue_head_t wqh;
+	struct list_head contexts;
+	spinlock_t lock;
+	unsigned long current_child;
+	unsigned long ino;
+	struct kref kref;
+	int count;
+	int uncontested;
+};
+
+struct transact_cdev {
+	dev_t devnum;
+	struct cdev cdev;
+	spinlock_t lock;
+	struct list_head ctx;
+};
+
+static struct transact_cdev g_cdev;
+
+static void transact_notify_death(struct transact_proc *p)
+{
+	struct transact_ctx *ctx = p->ctx;
+	spin_lock_irq(&ctx->lock);
+	if (ctx->uncontested) {
+		// Other side already dead.
+		spin_unlock_irq(&ctx->lock);
+		return;
+	}
+	ctx->current_child = !p->index;
+	ctx->uncontested = 1;
+	spin_unlock_irq(&ctx->lock);
+
+	spin_lock_irq(&ctx->wqh.lock);
+	// Wake up other process, if waiting.
+	if (waitqueue_active(&ctx->wqh))
+		wake_up_locked_poll(&ctx->wqh, POLLHUP);
+	spin_unlock_irq(&ctx->wqh.lock);
+}
+
+static ssize_t transact_switch_locked(struct transact_proc *p)
+{
+	struct transact_ctx *ctx = p->ctx;
+	ssize_t res = 0;
+	DECLARE_WAITQUEUE(wait, current);
+
+	if (ctx->uncontested) {
+		res = ctx->uncontested;
+		spin_unlock_irq(&ctx->lock);
+		return res;
+	}
+	ctx->current_child = !p->index;
+	spin_unlock_irq(&ctx->lock);
+
+	spin_lock_irq(&ctx->wqh.lock);
+	// Wake up other process, if waiting.
+	if (waitqueue_active(&ctx->wqh))
+		wake_up_locked_poll(&ctx->wqh, POLLIN);
+
+	// And sleep until it is our turn.
+	__add_wait_queue(&ctx->wqh, &wait);
+	for (;;) {
+		set_current_state(TASK_INTERRUPTIBLE);
+		if (ctx->current_child == p->index || ctx->uncontested) {
+			res = ctx->uncontested;
+			break;
+		}
+		if (signal_pending(current)) {
+			res = -ERESTARTSYS;
+			break;
+		}
+		spin_unlock_irq(&ctx->wqh.lock);
+		schedule();
+		spin_lock_irq(&ctx->wqh.lock);
+	}
+	__remove_wait_queue(&ctx->wqh, &wait);
+	__set_current_state(TASK_RUNNING);
+	spin_unlock_irq(&ctx->wqh.lock);
+
+	return res;
+}
+
+static struct transact_ctx* transact_get_ctx(struct transact_cdev *dev,
+		unsigned long ino)
+{
+	struct list_head* ctx_node;
+	struct transact_ctx* ctx = NULL;
+
+	spin_lock_irq(&dev->lock);
+	// Try to get the context from the list of previously created contexts.
+	list_for_each(ctx_node, &dev->ctx) {
+		ctx = container_of(ctx_node, struct transact_ctx, contexts);
+		if (ctx->ino == ino) {
+			// And increase the reference count in case another process tries to
+			// destroy it.
+			kref_get(&ctx->kref);
+			spin_unlock_irq(&dev->lock);
+			return ctx;
+		}
+	}
+	// There is no previously created context. Allocate a new one.
+	ctx = kmalloc(sizeof(struct transact_ctx), GFP_ATOMIC);
+	if (ctx) {
+		memset(ctx, 0, sizeof(struct transact_ctx));
+		kref_init(&ctx->kref);
+		ctx->current_child = -1;
+		ctx->dev = dev;
+		ctx->ino = ino;
+		spin_lock_init(&ctx->lock);
+		init_waitqueue_head(&ctx->wqh);
+		list_add(&ctx->contexts, &dev->ctx);
+	}
+	spin_unlock_irq(&dev->lock);
+
+	return ctx;
+}
+
+static void transact_release_ctx_locked(struct kref *ref)
+{
+	struct transact_ctx *ctx = container_of(ref, struct transact_ctx, kref);
+	list_del(&ctx->contexts);
+	kfree(ctx);
+}
+
+int transact_open(struct inode *inode, struct file *filp)
+{
+	struct transact_cdev *dev;
+	struct transact_ctx *ctx;
+	struct transact_proc *p;
+	int current_child;
+
+	dev = container_of(inode->i_cdev, struct transact_cdev, cdev);
+	ctx = transact_get_ctx(dev, inode->i_ino);
+	if (!ctx) {
+		return -ENOMEM;
+	}
+
+	spin_lock_irq(&ctx->lock);
+	if (ctx->count == 2) {
+		spin_unlock_irq(&ctx->lock);
+
+		spin_lock_irq(&dev->lock);
+		kref_put(&ctx->kref, transact_release_ctx_locked);
+		spin_unlock_irq(&dev->lock);
+
+		return -EBUSY;
+	}
+	current_child = ctx->count++;
+
+	p = &ctx->child[current_child];
+	p->ctx = ctx;
+	p->index = current_child;
+	filp->private_data = p;
+
+	// Passing in the write flag means that this process is Main and should pass
+	// through unobstructed. The other process should take the switch to block
+	// until Main has reached its first switch point, unless it already has, so
+	// it's the turn of the child.
+	if ((filp->f_mode & FMODE_WRITE) == 0 && ctx->current_child != current_child) {
+		transact_switch_locked(p);  // releases ctx->lock.
+	} else {
+		spin_unlock_irq(&ctx->lock);
+	}
+
+	return 0;
+}
+
+ssize_t transact_read(struct file *filp, char __user *buf, size_t count,
+		loff_t *ppos)
+{
+	struct transact_proc *p = (struct transact_proc *)filp->private_data;
+	ssize_t res;
+	__u64 data = 0;
+
+	if (count < sizeof(data))
+		return -EINVAL;
+	spin_lock_irq(&p->ctx->lock);
+	res = transact_switch_locked(p);  // releases ctx->lock.
+	if (res < 0)
+		return res;
+	data = res;
+
+	return put_user(data, (__u64 __user *) buf) ? -EFAULT : sizeof(data);
+}
+
+int transact_release(struct inode *inode, struct file *filp)
+{
+	struct transact_proc *p = (struct transact_proc *)filp->private_data;
+	struct transact_ctx *ctx = p->ctx;
+
+	transact_notify_death(p);
+	spin_lock_irq(&ctx->dev->lock);
+	kref_put(&ctx->kref, transact_release_ctx_locked);
+	spin_unlock_irq(&ctx->dev->lock);
+
+	return 0;
+}
+
+static struct file_operations transact_fops = {
+	.owner = THIS_MODULE,
+	.open = transact_open,
+	.read = transact_read,
+	.release = transact_release,
+};
+
+/* Initialize the LKM */
+__init int init_module()
+{
+	int err;
+	
+	memset(&g_cdev, 0, sizeof(g_cdev));
+	g_cdev.devnum = MKDEV(2038, 0);
+	err = register_chrdev_region(g_cdev.devnum, 1, "transact");
+	if (err < 0) {
+		printk(KERN_WARNING "Failed to allocate major/minor numbers\n");
+		return err;
+	}
+
+	cdev_init(&g_cdev.cdev, &transact_fops);
+	g_cdev.cdev.owner = THIS_MODULE;
+	g_cdev.cdev.ops = &transact_fops;
+	spin_lock_init(&g_cdev.lock);
+	INIT_LIST_HEAD(&g_cdev.ctx);
+	err = cdev_add(&g_cdev.cdev, g_cdev.devnum, 1);
+	if (err < 0) {
+		printk(KERN_WARNING "Failed to add cdev\n");
+		goto unregister;
+	}
+
+  return 0;
+
+unregister:
+	unregister_chrdev_region(g_cdev.devnum, 1);
+	return err;
+}
+
+__exit void cleanup_module()
+{
+	cdev_del(&g_cdev.cdev);
+	unregister_chrdev_region(g_cdev.devnum, 1);
+}
+
+MODULE_AUTHOR("lhchavez");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("A named semaphore for two processes with death notification.");
