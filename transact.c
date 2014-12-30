53b2e6a3ace42a795c49fcdf00f9703b2cb8d7f6
lhchavez <lhchavez@lhchavez.com>
2014-12-30 10:53:03 -0800

    Better read() semantics, write() support
    Now instead of read() filling in whether the other process has released
    the reference to the transactfd, it now returns 0 to signal that the
    file is now closed. This allows to fill in a shared value that the other
    process can modify by using write().

--- transact.c
+++ transact.c
@@ -25,17 +25,19 @@ struct transact_cdev;
 struct transact_proc {
 	struct transact_ctx *ctx;
 	int index;
+	int initialized;
 };
 
 struct transact_ctx {
 	struct transact_proc child[2];
 	struct transact_cdev *dev;
+	__u64 value;
 	wait_queue_head_t wqh;
 	struct list_head contexts;
 	spinlock_t lock;
-	unsigned long current_child;
 	unsigned long ino;
 	struct kref kref;
+	int current_child;
 	int count;
 	int uncontested;
 };
@@ -69,14 +71,14 @@ static void transact_notify_death(struct transact_proc *p)
 	spin_unlock_irq(&ctx->wqh.lock);
 }
 
-static ssize_t transact_switch_locked(struct transact_proc *p)
+static int transact_switch_locked(struct transact_proc *p)
 {
 	struct transact_ctx *ctx = p->ctx;
-	ssize_t res = 0;
+	int res = 0;
 	DECLARE_WAITQUEUE(wait, current);
 
 	if (ctx->uncontested) {
-		res = ctx->uncontested;
+		res = -EDEADLOCK;
 		spin_unlock_irq(&ctx->lock);
 		return res;
 	}
@@ -93,7 +95,8 @@ static ssize_t transact_switch_locked(struct transact_proc *p)
 	for (;;) {
 		set_current_state(TASK_INTERRUPTIBLE);
 		if (ctx->current_child == p->index || ctx->uncontested) {
-			res = ctx->uncontested;
+			if (ctx->uncontested)
+				res = -EDEADLOCK;
 			break;
 		}
 		if (signal_pending(current)) {
@@ -114,13 +117,11 @@ static ssize_t transact_switch_locked(struct transact_proc *p)
 static struct transact_ctx* transact_get_ctx(struct transact_cdev *dev,
 		unsigned long ino)
 {
-	struct list_head* ctx_node;
 	struct transact_ctx* ctx = NULL;
 
 	spin_lock_irq(&dev->lock);
 	// Try to get the context from the list of previously created contexts.
-	list_for_each(ctx_node, &dev->ctx) {
-		ctx = container_of(ctx_node, struct transact_ctx, contexts);
+	list_for_each_entry(ctx, &dev->ctx, contexts) {
 		if (ctx->ino == ino) {
 			// And increase the reference count in case another process tries to
 			// destroy it.
@@ -177,21 +178,12 @@ int transact_open(struct inode *inode, struct file *filp)
 		return -EBUSY;
 	}
 	current_child = ctx->count++;
+	spin_unlock_irq(&ctx->lock);
 
 	p = &ctx->child[current_child];
 	p->ctx = ctx;
 	p->index = current_child;
 	filp->private_data = p;
-
-	// Passing in the write flag means that this process is Main and should pass
-	// through unobstructed. The other process should take the switch to block
-	// until Main has reached its first switch point, unless it already has, so
-	// it's the turn of the child.
-	if ((filp->f_mode & FMODE_WRITE) == 0 && ctx->current_child != current_child) {
-		transact_switch_locked(p);  // releases ctx->lock.
-	} else {
-		spin_unlock_irq(&ctx->lock);
-	}
 
 	return 0;
 }
@@ -201,17 +193,59 @@ ssize_t transact_read(struct file *filp, char __user *buf, size_t count,
 {
 	struct transact_proc *p = (struct transact_proc *)filp->private_data;
 	ssize_t res;
-	__u64 data = 0;
+	__u64 data;
 
 	if (count < sizeof(data))
 		return -EINVAL;
 	spin_lock_irq(&p->ctx->lock);
-	res = transact_switch_locked(p);  // releases ctx->lock.
-	if (res < 0)
+	res = transact_switch_locked(p);  // releases p->ctx->lock.
+	data = p->ctx->value;
+	if (res == -EDEADLOCK) {
+		// Special case. EDEADLOCK means the other process has already closed the
+		// file, so return 0 to make the caller think the file is closed.
+		return 0;
+	} else if (res < 0) {
 		return res;
-	data = res;
+	}
 
-	return put_user(data, (__u64 __user *) buf) ? -EFAULT : sizeof(data);
+	return put_user(data, (__u64 __user *)buf) ? -EFAULT : sizeof(data);
+}
+
+ssize_t transact_write(struct file *filp, const char __user *buf, size_t count,
+		loff_t *ppos)
+{
+	struct transact_proc *p = (struct transact_proc *)filp->private_data;
+	__u64 data;
+	int res;
+
+	if (count < sizeof(data))
+		return -EINVAL;
+	if (get_user(data, (const __u64 __user *)buf))
+		return -EFAULT;
+
+	if (unlikely(!p->initialized)) {
+		p->initialized = 1;
+		// Passing nonzero to the first write means that this process is Main and
+		// should pass through unobstructed. The other process should take the
+		// switch to block until Main has reached its first switch point, unless
+		// Main already has, in which case it's the turn of the child.
+		spin_lock_irq(&p->ctx->lock);
+		if (!data && p->ctx->current_child != p->index) {
+			res = transact_switch_locked(p);  // releases ctx->lock.
+			if (res == -EDEADLOCK)
+				return 0;
+			else if (res < 0)
+				return res;
+		} else {
+			spin_unlock_irq(&p->ctx->lock);
+		}
+	} else {
+		spin_lock_irq(&p->ctx->lock);
+		p->ctx->value = data;
+		spin_unlock_irq(&p->ctx->lock);
+	}
+
+	return sizeof(data);
 }
 
 int transact_release(struct inode *inode, struct file *filp)
@@ -231,6 +265,7 @@ static struct file_operations transact_fops = {
 	.owner = THIS_MODULE,
 	.open = transact_open,
 	.read = transact_read,
+	.write = transact_write,
 	.release = transact_release,
 };
 
